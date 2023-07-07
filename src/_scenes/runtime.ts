4fe3d9647f922012e1b5ac7f485af0d3413435d6
CMJ <chemingjun@kuaishou.com>
2023-07-07 15:41:56 +0800

    scenes

--- src/_scenes/runtime.ts
+++ src/_scenes/runtime.ts
@@ -1,8 +1,4 @@
 export default function ({env, data, inputs, _inputs, _inputsCallable}) {
-//   _inputs['_open'](() => {
-// debugger
-//   })
-
   inputs['open'](params => {
     env.openScene(data.sceneId, params,data.openType)
 
