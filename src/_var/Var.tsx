dd3ee8b105771c05023bda981931d6578c2786d8
CMJ <chemingjun@kuaishou.com>
2023-07-24 18:35:36 +0800

    upgrade var

--- src/_var/Var.tsx
+++ src/_var/Var.tsx
@@ -1,4 +1,4 @@
-export default function ({env, data, outputs, inputs,_notifyBindings}) {
+export default function ({env, data, outputs, inputs, _notifyBindings}) {
   inputs['get']((val, relOutpus) => {
     const nowVal = data.val !== void 0 ? data.val : data.initValue
     const cv = clone(nowVal)
@@ -6,11 +6,13 @@ export default function ({env, data, outputs, inputs,_notifyBindings}) {
     relOutpus['return'](cv)
   })
 
-  inputs['set'](val => {
+  inputs['set']((val, relOutpus) => {
     data.val = val
     const cVal = clone(val)
     outputs['changed'](cVal, true)//notify all forked coms
     _notifyBindings(cVal)
+
+    relOutpus['return'](cVal)
   })
 
   inputs['reset'](() => {
