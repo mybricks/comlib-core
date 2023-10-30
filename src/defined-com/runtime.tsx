75c85d0ffc27b2c556ba858cda9177febb9941ff
leon-js <42924541+leon-js@users.noreply.github.com>
2023-10-30 19:35:56 +0800

    fix

--- src/defined-com/runtime.tsx
+++ src/defined-com/runtime.tsx
@@ -13,17 +13,19 @@ export default function ({ env, data, inputs: propsInputs, outputs: propsOutputs
     const { toJSON } = data
     return env.renderCom(toJSON, {
       ref(refs) {
-        const { inputs, outputs } = toJSON
+        if (env.runtime) {
+          const { inputs, outputs } = toJSON
 
-        inputs.forEach(({ id }) => {
-          propsInputs[id]?.((value) => {
-            refs.inputs[id](value)
+          inputs.forEach(({ id }) => {
+            propsInputs[id]?.((value) => {
+              refs.inputs[id](value)
+            })
           })
-        })
-
-        outputs.forEach(({ id }) => {
-          refs.outputs(id, propsOutputs[id]);
-        })
+  
+          outputs.forEach(({ id }) => {
+            refs.outputs(id, propsOutputs[id]);
+          })
+        }
       },
       /** 禁止主动触发IO、执行自执行计算组件 */
       disableAutoRun: true
