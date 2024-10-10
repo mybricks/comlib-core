7ae6526c8bf8c3c9e711b27667f0b83856e00e67
leon-js <42924541+leon-js@users.noreply.github.com>
2024-10-10 17:52:25 +0800

    feat(var):clone逻辑优化

--- src/_var/Var.tsx
+++ src/_var/Var.tsx
@@ -27,19 +27,49 @@ export default function ({env, data, outputs, inputs, _notifyBindings}) {
   // outputs['changed'](data.val)
 }
 
-function clone(val) {
-  if (val) {
-    try {
-      const type = Object.prototype.toString.call(val);
-
-      if (['[object Object]', '[object Array]'].includes(type)) {
-        return JSON.parse(JSON.stringify(val))
-      }
-
-      return val
-    } catch (ex) {
-      return val
+function clone(obj: any, cache: any = []) {
+  try {
+    const type = Object.prototype.toString.call(obj)
+    if (obj === null || typeof obj !== 'object' || type.startsWith('[object HTML') || type === '[object FormData]') {
+      return obj
     }
+
+    const hit = cache.filter((i: any) => i.original === obj)[0]
+
+    if (hit) {
+      return hit.copy
+    }
+
+    const copy: any = Array.isArray(obj) ? [] : {}
+
+    cache.push({
+      original: obj,
+      copy
+    })
+
+    Object.keys(obj).forEach(key => {
+      copy[key] = clone(obj[key], cache)
+    })
+
+    return copy
+  } catch (ex) {
+    return obj
   }
-  return val
 }
+
+// function clone(val) {
+//   if (val) {
+//     try {
+//       const type = Object.prototype.toString.call(val);
+
+//       if (['[object Object]', '[object Array]'].includes(type)) {
+//         return JSON.parse(JSON.stringify(val))
+//       }
+
+//       return val
+//     } catch (ex) {
+//       return val
+//     }
+//   }
+//   return val
+// }
