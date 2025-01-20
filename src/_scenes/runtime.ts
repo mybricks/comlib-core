651ee47b7b8af3b6f536adbcbb834f6fc3925eb8
leon-js <42924541+leon-js@users.noreply.github.com>
2025-01-20 16:20:09 +0800

    fix

--- src/_scenes/runtime.ts
+++ src/_scenes/runtime.ts
@@ -1,7 +1,10 @@
 export default function ({env, data, inputs, _inputs, _inputsCallable}) {
-  inputs['open'](params => {
-    env.canvas.open(data._sceneId, params,data._sceneShowType === 'popup' ? null : data.openType)
-
-    _inputsCallable['_open'](params)
+  inputs[data._pinId || 'open'](params => {
+    if (!data._pinId || data._pinId === 'open') {
+      env.canvas.open(data._sceneId, params,data._sceneShowType === 'popup' ? null : data.openType)
+      _inputsCallable["_open"](params)
+    } else {
+      _inputsCallable[data._pinId](params)
+    }
   })
 }
