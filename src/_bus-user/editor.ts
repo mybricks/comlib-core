8a734ed489779de51ef2df65187b3dab347fa217
liubiao03 <liubiao03@kuaishou.com>
2025-07-24 20:22:32 +0800

    feat: 实现获取用户的总线

--- src/_bus-user/editor.ts
+++ src/_bus-user/editor.ts
@@ -1,15 +1,3 @@
 export default {
-  '@init': ({ setDesc }) => {
-    setDesc(`（领域模型为空）`);
-  },
-  "@domainModel": {
-    get({ data }) {
-      return data.model;
-    },
-    set({ data, setDesc }, model) {
-      data.model = model;
-      setDesc(`已选择：${model.title}`);
-    },
-  },
   ':root': [],
 };
--- src/_bus-user/runtime.ts
+++ src/_bus-user/runtime.ts
@@ -1,33 +1,13 @@
 export default function ({ env, data, inputs }) {
-  inputs["call"]((value, relOutputs) => {
-    if (!data.model) {
-      relOutputs["catch"]("没有选择领域模型");
+  inputs["call"]((inputValue, relOutputs) => {
+    if (!env?.getBusData) {
+      relOutputs["catch"]("未实现env.getBusData");
       return;
     }
-
-    if (!env.callDomainModel) {
-      relOutputs["catch"]("未实现env.callDomainModel");
-      return;
-    }
-
-    // 临时测试
-    env.callDomainModel({
-      // 模型信息
-      model: data.model,
-      // 参数
-      params: value,
-      configs: {
-        callType: "call",
-      }
-    }, (error, loading, output) => {
-      if (loading) {
-        return
-      }
-      if (error) {
-        relOutputs["catch"](error);
-      } else {
-        relOutputs["then"](output);
-      }
-    });
+    env.getBusData('user', inputValue)?.then?.(result => {
+      relOutputs["then"](result);
+    }).catch(err => {
+      relOutputs["catch"](err?.message ?? '未知错误');
+    })
   });
 };
